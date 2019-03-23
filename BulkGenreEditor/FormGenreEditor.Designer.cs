1e392501fb507cf79b4649d74951008311d766f8
Costin <ssj_costy@yahoo.com>
2019-03-23 21:31:38 +0000

    Added a small note in the "Bulk Add/Remove Games" dialog form.

--- BulkGenreEditor/FormGenreEditor.Designer.cs
+++ BulkGenreEditor/FormGenreEditor.Designer.cs
@@ -36,7 +36,10 @@
             this.txtCustomGenre = new System.Windows.Forms.TextBox();
             this.btnCustomGenre = new System.Windows.Forms.Button();
             this.picLoader = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.picLoader)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblInstructions
@@ -51,7 +54,7 @@
             // btnRemoveGenres
             // 
             this.btnRemoveGenres.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnRemoveGenres.Location = new System.Drawing.Point(415, 363);
+            this.btnRemoveGenres.Location = new System.Drawing.Point(415, 438);
             this.btnRemoveGenres.Name = "btnRemoveGenres";
             this.btnRemoveGenres.Size = new System.Drawing.Size(129, 32);
             this.btnRemoveGenres.TabIndex = 2;
@@ -62,7 +65,7 @@
             // btnAddGenres
             // 
             this.btnAddGenres.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.btnAddGenres.Location = new System.Drawing.Point(12, 363);
+            this.btnAddGenres.Location = new System.Drawing.Point(12, 438);
             this.btnAddGenres.Name = "btnAddGenres";
             this.btnAddGenres.Size = new System.Drawing.Size(129, 32);
             this.btnAddGenres.TabIndex = 3;
@@ -87,9 +90,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(12, 296);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(139, 17);
+            this.label2.Size = new System.Drawing.Size(136, 17);
             this.label2.TabIndex = 6;
-            this.label2.Text = "Add a custom genre:";
+            this.label2.Text = "Create a new genre:";
             // 
             // txtCustomGenre
             // 
@@ -114,19 +117,48 @@
             // 
             // picLoader
             // 
+            this.picLoader.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.picLoader.Image = global::BulkGenreEditor.Properties.Resources.green_loading_bar;
-            this.picLoader.Location = new System.Drawing.Point(201, 366);
+            this.picLoader.Location = new System.Drawing.Point(201, 441);
             this.picLoader.Name = "picLoader";
             this.picLoader.Size = new System.Drawing.Size(160, 24);
             this.picLoader.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.picLoader.TabIndex = 9;
             this.picLoader.TabStop = false;
             // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(519, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adding genres will not overwrite your selected games\' existing genres, but will o" +
+    "nly append the new ones to each selected game. However, be careful with the remo" +
+    "ve operation.";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(11, 344);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(532, 88);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Note";
+            // 
             // FormGenreEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(556, 407);
+            this.ClientSize = new System.Drawing.Size(556, 482);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.picLoader);
             this.Controls.Add(this.btnCustomGenre);
             this.Controls.Add(this.txtCustomGenre);
@@ -140,6 +172,7 @@
             this.Text = "Bulk Add/Remove Genres";
             this.Load += new System.EventHandler(this.FormGenreEditor_Load);
             ((System.ComponentModel.ISupportInitialize)(this.picLoader)).EndInit();
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -155,5 +188,7 @@
         private System.Windows.Forms.TextBox txtCustomGenre;
         private System.Windows.Forms.Button btnCustomGenre;
         private System.Windows.Forms.PictureBox picLoader;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
