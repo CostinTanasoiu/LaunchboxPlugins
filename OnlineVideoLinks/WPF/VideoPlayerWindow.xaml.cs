fc2f0787dfd641a659329272674de537cb8f1dca
SsjCosty <ssj.costy@gmail.com>
2020-11-30 17:46:50 +0000

    Improved video player window

--- OnlineVideoLinks/WPF/VideoPlayerWindow.xaml.cs
+++ OnlineVideoLinks/WPF/VideoPlayerWindow.xaml.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
+using System.Windows.Input;
 
 namespace OnlineVideoLinks.WPF
 {
@@ -24,24 +25,8 @@ namespace OnlineVideoLinks.WPF
             _mediaPlayer = new MediaPlayer(_libVLC);
             vlcView.MediaPlayer = _mediaPlayer;
 
-            vlcView.KeyUp += VlcView_KeyUp;
-        }
-
-        private void VlcView_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
-        {
-            MessageBox.Show("Pressed key");
-        }
-
-        public void SkipForward()
-        {
-            _mediaPlayer.Time += 10000;
-        }
-
-        public void SkipBackward()
-        {
-            if (_mediaPlayer.Time > 10000)
-                _mediaPlayer.Time -= 10000;
-            else _mediaPlayer.Time = 0;
+            _mediaPlayer.TimeChanged += _mediaPlayer_TimeChanged;
+            _mediaPlayer.EndReached += _mediaPlayer_EndReached;
         }
 
         private void Window_ContentRendered(object sender, EventArgs e)
@@ -59,9 +44,26 @@ namespace OnlineVideoLinks.WPF
             _libVLC.Dispose();
         }
 
-        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        public void SkipForward()
+        {
+            _mediaPlayer.Time += 10000;
+        }
+
+        public void SkipBackward()
+        {
+            if (_mediaPlayer.Time > 10000)
+                _mediaPlayer.Time -= 10000;
+            else _mediaPlayer.Time = 0;
+        }
+
+        public void PlayPause()
         {
             _mediaPlayer.Pause();
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            PlayPause();
         }
 
         private void btnStop_Click(object sender, RoutedEventArgs e)
@@ -77,6 +79,45 @@ namespace OnlineVideoLinks.WPF
         private void btnSkipFwd_Click(object sender, RoutedEventArgs e)
         {
             SkipForward();
+        }
+
+        private void Window_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Enter:
+                case Key.Space:
+                case Key.A:
+                    PlayPause(); break;
+                case Key.Escape:
+                case Key.B:
+                    this.Close(); break;
+                case Key.Left:
+                    SkipBackward(); break;
+                case Key.Right:
+                    SkipForward(); break;
+            }
+        }
+
+        private void _mediaPlayer_TimeChanged(object sender, MediaPlayerTimeChangedEventArgs e)
+        {
+            var current = TimeSpan.FromMilliseconds(_mediaPlayer.Time);
+            var total = TimeSpan.FromMilliseconds(_mediaPlayer.Length);
+
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                txtProgress.Text = $"{TimespanFormat(current)} / {TimespanFormat(total)}";
+            }));
+        }
+
+        private void _mediaPlayer_EndReached(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private string TimespanFormat(TimeSpan t)
+        {
+            return t.Hours > 0 ? t.ToString("hh\\:mm\\:ss") : t.ToString("mm\\:ss");
         }
     }
 }
