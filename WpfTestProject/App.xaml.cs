3696be4ff5ecce08cdcc1e8e579f7b78730c534d
Costin Tanasoiu <costin.tanasoiu@gmail.com>
2024-07-07 12:53:48 +0100

    Updated hardcoded LaunchBox directory paths

--- WpfTestProject/App.xaml.cs
+++ WpfTestProject/App.xaml.cs
@@ -39,37 +39,37 @@ namespace WpfTestProject
                     new AdditionalApplicationMock
                     {
                         Name = "Video: Longplay",
-                        ApplicationPath = @"C:\Programs\LaunchBox\ThirdParty\VLC\x64\vlc.exe",
+                        ApplicationPath = @"C:\Games\LaunchBox\ThirdParty\VLC\x64\vlc.exe",
                         CommandLine = "https://youtu.be/yka30n1D6L0"
                     },
                     new AdditionalApplicationMock
                     {
                         Name = "Video: Is Death and Return of Superman Worth Playing Today?",
-                        ApplicationPath = @"C:\Programs\LaunchBox\ThirdParty\VLC\x64\vlc.exe",
+                        ApplicationPath = @"C:\Games\LaunchBox\ThirdParty\VLC\x64\vlc.exe",
                         CommandLine = "https://www.youtube.com/watch?v=7hfYthWLJsA"
                     },
                     new AdditionalApplicationMock
                     {
                         Name = "Video: Big Buck Bunny",
-                        ApplicationPath = @"C:\Programs\LaunchBox\ThirdParty\VLC\x64\vlc.exe",
+                        ApplicationPath = @"C:\Games\LaunchBox\ThirdParty\VLC\x64\vlc.exe",
                         CommandLine = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
                     },
                     new AdditionalApplicationMock
                     {
                         Name = "Video: Steam Video",
-                        ApplicationPath = @"C:\Programs\LaunchBox\ThirdParty\VLC\x64\vlc.exe",
+                        ApplicationPath = @"C:\Games\LaunchBox\ThirdParty\VLC\x64\vlc.exe",
                         CommandLine = "https://cdn.cloudflare.steamstatic.com/steam/apps/256790115/movie480_vp9.webm?t=1592821021"
                     },
                     new AdditionalApplicationMock
                     {
                         Name = "Video: Longplay",
-                        ApplicationPath = @"C:\Programs\LaunchBox\ThirdParty\VLC\x64\vlc.exe",
+                        ApplicationPath = @"C:\Games\LaunchBox\ThirdParty\VLC\x64\vlc.exe",
                         CommandLine = "https://youtu.be/yka30n1D6L0"
                     },
                     new AdditionalApplicationMock
                     {
                         Name = "Video: Is Death and Return of Superman Worth Playing Today?",
-                        ApplicationPath = @"C:\Programs\LaunchBox\ThirdParty\VLC\x64\vlc.exe",
+                        ApplicationPath = @"C:\Games\LaunchBox\ThirdParty\VLC\x64\vlc.exe",
                         CommandLine = "https://youtu.be/7hfYthWLJsA"
                     }
                 }
