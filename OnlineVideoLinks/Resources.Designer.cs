a1c993c2e9163f3198ec3c46b3cd021ac21cebda
Costin Tanasoiu <costin.tanasoiu@gmail.com>
2026-04-07 11:30:06 +0100

    Removed unnecessary resources

--- OnlineVideoLinks/Resources.Designer.cs
+++ OnlineVideoLinks/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace OnlineVideoLinks {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -79,39 +79,9 @@ namespace OnlineVideoLinks {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap icons8_add_48 {
-            get {
-                object obj = ResourceManager.GetObject("icons8-add-48", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap Video {
             get {
                 object obj = ResourceManager.GetObject("Video", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap XboxOne_A {
-            get {
-                object obj = ResourceManager.GetObject("XboxOne_A", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap XboxOne_B {
-            get {
-                object obj = ResourceManager.GetObject("XboxOne_B", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
